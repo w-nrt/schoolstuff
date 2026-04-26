d5113d9503d3471a3342db5d96f261f5ae2a573b
winter <48132954+w-nrt@users.noreply.github.com>
2026-04-26 23:04:58 +0200

    Enhance input handling and output clarity
    Increased line buffer size for reading input and improved output messages for better clarity.

--- LireEtinterpreter.c
+++ LireEtinterpreter.c
@@ -1,3 +1,11 @@
+/* Ce code sert à lire un fichier du type Hello.txt (qu'on peut trouver sur le GitHub https://github.com/w-nrt/schoolstuff) et d'essayer d'executer toute les instructions.
+La façon dont il marche est la suivante: 
+--Interpreter chaque instruction: et les stocker dans un tableau "d'étapes" qui est un enregistrement composé d'actions, de preconditions, d'ajouts et de suppression
+--On fait l'inventaire des choses qu'on a déjà et de ce dont on a besoin à la fin
+--On execute chaque étape, si une étape échoue on passe à la suivante puis on recommence
+--Chaque fois, on vérifie l'inventaire pour trouver nos pre-réquis stockés dans "Final", si on les trouve tous l'execution est un succés, sinon au bout d'un moment on arrête la boucle d'éxecution
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -11,7 +19,7 @@
 // typedefs
 typedef char string[1024];
 
-typedef struct etape    // là où on va stocker les conditions
+typedef struct etape // là où on va stocker les conditions
 {
 
     string actions[MAX_CONDITIONS];
@@ -26,7 +34,7 @@ typedef struct etape    // là où on va stocker les conditions
     string delete[MAX_CONDITIONS];
     int nbDelete;
 
-    bool complete;  // permettra de savoir si on est déjà passé par une condition
+    bool complete; // permettra de savoir si on est déjà passé par une condition
 } Etape;
 
 // variables globales
@@ -34,11 +42,12 @@ int m;
 string Start[MAX_CONDITIONS], Finish[MAX_CONDITIONS], Inventory[MAX_INVENTORY]; // Stockage du start, du finish et de l'inventaire
 Etape rules[MAX_RULES];
 bool peutExecuter;
-char c[100];
+char c[200]; // On augmente la capacité pour les lignes plus longues 100-->200  
 int nEtapes = -1;
 int m;
 
-void printinventaire(void) // Fonction qui affiche notre inventaire pour mieux visualiser.
+void printinventaire(void)
+{ // Fonction qui affiche notre inventaire pour mieux visualiser.
     printf("Dans l'inventaire on a: ");
     for (int i = 0; i < MAX_INVENTORY; i++)
     {
@@ -78,10 +87,10 @@ int searchRemoveString(string source[], string target, int n)
 
 void afficherRules(int nbRules) // Fonction pour afficher les règles, cette fonction sert simplement de visualisation
 {
-    printf("\n=== AFFICHAGE DES REGLES ===\n");
+    printf("\nAffichage des etapes: \n");
     for (int i = 0; i < nbRules; i++)
     {
-        printf("--- REGLE %d ---\n", i + 1);
+        printf("--- ETAPE NUMERO %d ---\n", i + 1);
         printf("  Actions: ");
         for (int j = 0; j < rules[i].nbActions; j++)
             printf("[%s] ", rules[i].actions[j]);
@@ -125,24 +134,24 @@ int parseLine(char source[], string cible[])
 bool Execution(int i) // Fonction qui sert à appliquer chaque rule et verifier si elle est applicable ou pas
 {
     if (rules[i].complete)
-        return true;    // Si on a réussi à compléter l'étape
+        return true; // Si on a réussi à compléter l'étape
 
     peutExecuter = true;
     for (int p = 0; p < rules[i].nbPreconds; p++)
     {
         if (!TrouveString(Inventory, MAX_INVENTORY, rules[i].preconds[p]))
         {
-            peutExecuter = false;   // Si dans notre inventaire il nous manque des préconditions de l'étape, alors cette étape ne peut pas être éxécutée.
+            peutExecuter = false; // Si dans notre inventaire il nous manque des préconditions de l'étape, alors cette étape ne peut pas être éxécutée.
         }
     }
 
-    if (peutExecuter)               // Si notre inventaire possède tout les preconds de l'étape actuelle
+    if (peutExecuter) // Si notre inventaire possède tout les preconds de l'étape actuelle
     {
-        printf("\n---- EXECUTION DE LA RULE : %d ----\n", i);
+        printf("\n-- EXECUTION DE L'ETAPE : %d --\n", i);
 
         for (int j = 0; j < rules[i].nbDelete; j++)
         {
-            searchRemoveString(Inventory, rules[i].delete[j], MAX_INVENTORY);   // On supprime de notre inventaire les "delete" de l'étape.
+            searchRemoveString(Inventory, rules[i].delete[j], MAX_INVENTORY); // On supprime de notre inventaire les "delete" de l'étape.
         }
 
         for (int k = 0; k < rules[i].nbAdd; k++)
@@ -152,7 +161,7 @@ bool Execution(int i) // Fonction qui sert à appliquer chaque rule et verifier 
                 m++; // Trouver une case vide
             if (m < MAX_INVENTORY)
             {
-                strcpy(Inventory[m], rules[i].adds[k]);     // On ajoute à notre inventaire les adds.
+                strcpy(Inventory[m], rules[i].adds[k]); // On ajoute à notre inventaire les adds.
             }
             else
             {
@@ -178,17 +187,19 @@ int main(void) // Fonction principale qui lis le .txt qu'on lui envoie et tente 
 
     int nLigne = 0;
 
-    while (fgets(c, 100, Fichier) != NULL)
+    while (fgets(c, 200, Fichier) != NULL) // On passe de 100 à 200 ici aussi
     {
         if (c[0] == '*')
             continue; // Ignorer les astérisques
 
-        if (nEtapes == -1)                  // On commence à une étape qui existe pas, c'est-à-dire le Start et le Finish qui ne sont pas des étapes.
+        if (nEtapes == -1) // On commence à une étape qui existe pas, c'est-à-dire le Start et le Finish qui ne sont pas des étapes.
         {
             if (nLigne == 0)
-                parseLine(c, Start);        // Récupère le Start
+                parseLine(c, Start); // Récupère le Start
             else if (nLigne == 1)
-                parseLine(c, Finish);       // Récupère le Finish
+            {
+                parseLine(c, Finish); // Récupère le Finish
+            }
             else
             {
                 nEtapes = 0;
@@ -197,24 +208,24 @@ int main(void) // Fonction principale qui lis le .txt qu'on lui envoie et tente 
             }
         }
 
-        if (nEtapes != -1)              // On entre dans les étapes, après le Start et le Finish donc, une étape se trouve entre deux lignes d'astérisques et contient "action", "preconds", "add" et "delete".
+        if (nEtapes != -1) // On entre dans les étapes, après le Start et le Finish donc, une étape se trouve entre deux lignes d'astérisques et contient "action", "preconds", "add" et "delete".
         {
             if (strncmp(c, "action:", 7) == 0)
             {
-                rules[nEtapes].nbActions = parseLine(c, rules[nEtapes].actions);    // Récupération des "action" de l'étape actuelle.
+                rules[nEtapes].nbActions = parseLine(c, rules[nEtapes].actions); // Récupération des "action" de l'étape actuelle.
             }
             else if (strncmp(c, "preconds:", 9) == 0)
             {
-                rules[nEtapes].nbPreconds = parseLine(c, rules[nEtapes].preconds);  // Récupération des "preconds" de l'étape actuelle.
+                rules[nEtapes].nbPreconds = parseLine(c, rules[nEtapes].preconds); // Récupération des "preconds" de l'étape actuelle.
             }
             else if (strncmp(c, "add:", 4) == 0)
             {
-                rules[nEtapes].nbAdd = parseLine(c, rules[nEtapes].adds);           // Récupération des "add" de l'étape actuelle.
+                rules[nEtapes].nbAdd = parseLine(c, rules[nEtapes].adds); // Récupération des "add" de l'étape actuelle.
             }
             else if (strncmp(c, "delete:", 7) == 0)
             {
-                rules[nEtapes].nbDelete = parseLine(c, rules[nEtapes].delete);      // Récupération des "delete" de l'étape actuelle.
-                rules[nEtapes].complete = false;                                    // Au départ, une action n'est jamais compléte.
+                rules[nEtapes].nbDelete = parseLine(c, rules[nEtapes].delete); // Récupération des "delete" de l'étape actuelle.
+                rules[nEtapes].complete = false;                               // Au départ, une action n'est jamais compléte.
                 nEtapes++;
             }
         }
@@ -223,66 +234,80 @@ int main(void) // Fonction principale qui lis le .txt qu'on lui envoie et tente 
     fclose(Fichier);
 
     // Initialisation de l'inventaire
-    printf("=== INIT INVENTAIRE ===\n");
+    printf("Initialisation Inventaire :\n");
     for (int i = 0; i < MAX_CONDITIONS; i++)
     {
         if (Start[i][0] != '\0')
         {
-            strcpy(Inventory[i], Start[i]);     // Au départ, notre inventaire est constitué du Start, il faut qu'à la fin notre inventaire soit égal au Finish.
+            strcpy(Inventory[i], Start[i]); // Au départ, notre inventaire est constitué du Start, il faut qu'à la fin notre inventaire soit égal au Finish.
         }
     }
 
     afficherRules(nEtapes);
     printinventaire();
 
-  // Boucle d'exécution
+    // Boucle d'exécution
     bool objectifAtteint = false;
+    bool blocage = false; 
 
-    while (!objectifAtteint)    // Tant que les preconds ne sont pas égales au finish, on répète cette partie de code
+    // boucle continue jusqu'à qu'on arrive à executer les instructions ou que celles-ci "bloquent"
+    while (!objectifAtteint && !blocage)
     {
         bool actionRealisee = false; // Sert à éviter que le programme cherche pour toujours
-        bool chercheObj = true;
 
-        for (int i = 0; i < nEtapes; i++)
+        // For multicondition pour qu'on puisse l'arrêter si besoin
+        for (int i = 0; i < nEtapes*2 && !objectifAtteint; i++)
         {
             if (!rules[i].complete) // Si on a pas complété les rules
             {
-                if (Execution(i))   // Si l'éxecution de cette étape s'est bien déroulée.
+                if (Execution(i)) // Si l'éxecution de cette étape s'est bien déroulée.
                 {
-                    actionRealisee = true; 
-                    
-   
-                    objectifAtteint = true; // true par défaut, set a false si non vrai
+                    actionRealisee = true;
+
+                    objectifAtteint = true; // true par défaut
                     m = 0;
-                    
-                    while (m < MAX_CONDITIONS && Finish[m][0] != '\0' && chercheObj)
+
+
+                    while (m < MAX_CONDITIONS && Finish[m][0] != '\0' && objectifAtteint)
                     {
+                        printf("\n JE VERIFIE SI: [%s] EST DANS L'INVENTAIRE\n", Finish[m]);
+
                         // Si on ne trouve pas une des conditions finales dans l'inventaire
                         if (!TrouveString(Inventory, MAX_INVENTORY, Finish[m]))
                         {
-                            objectifAtteint = false; 
-                            chercheObj = false; // On arrête la boucle de suite
+                            objectifAtteint = false; // Stoppe le while au prochain tour
                         }
                         m++;
                     }
 
-                    // Si on a toutes les conditions de Finish, on arrête tout immédiatement, sa permet d'éviter de revenir sur une étape, pouvant causer une boucle infinie alors qu'on a rempli le Finish.
                     if (objectifAtteint)
                     {
-                        printf("\n-- EXECUTION COMPLETE! SUCCES!\n");
-                        return 0; // on sort directement du programme, pas besoin de faire d'autre chose
+                        printf("\n- - EXECUTION COMPLETE! SUCCES! - -\n");
                     }
                 }
             }
         }
 
-        // c'est-à-dire qu'on est coincé, on ne peut appliquer plus de règles. dans ce cas on évite une boucle infinie en sortant
-        if (!actionRealisee)
+        // Si on est coincé (aucune règle n'a pu être appliquée ce tour-ci)
+        if (!actionRealisee && !objectifAtteint)
         {
             printf("\n! ! ! BLOCAGE : AUCUNE ACTION POSSIBLE. ECHEC ! ! !\n");
-            objectifAtteint = true; // sort de la boucle
+            blocage = true; // On sort de la boucle while et on passe à la suite
         }
     }
 
-    return 0;
+    printf("\n- - BILAN - -\n");
+    printinventaire();
+
+    printf("On voulait au minimum: ");
+    for (int i = 0; i < MAX_CONDITIONS; i++) //on affiche tout les pre requis pour l'execution
+    {
+        if (Finish[i][0] != '\0')
+        {
+            printf("[%s] ", Finish[i]);
+        }
+    }
+    printf("\n\n");
+
+return 0; // Tradition
 }
